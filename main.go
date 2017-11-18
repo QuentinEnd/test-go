2c817dfdf76ced4d454d0efc7d870eddf45fb99f
QuentinEnd <quentin.enard@gmx.fr>
2017-11-18 15:15:18 +0100

    Ajout destination ainsi qu'un personnage

--- main.go
+++ main.go
@@ -4,23 +4,26 @@ import (
 	tl "github.com/JoelOtter/termloop"
 )
 
+type Destination struct {
+	*tl.Entity
+}
+
 type Player struct {
 	*tl.Entity
 }
 
-func (player *Player) Tick(event tl.Event) {
-	if event.Type == tl.EventKey {
-		x, y := player.Position()
-		switch event.Key {
+func (destination *Destination) Tick(keyboard tl.Event) {
+	if keyboard.Type == tl.EventKey {
+		x, y := destination.Position()
+		switch keyboard.Key {
 		case tl.KeyArrowUp:
-			player.SetPosition(x, y-1)
+			destination.SetPosition(x, y-1)
 		case tl.KeyArrowDown:
-			player.SetPosition(x, y+1)
+			destination.SetPosition(x, y+1)
 		case tl.KeyArrowLeft:
-			player.SetPosition(x-1, y)
+			destination.SetPosition(x-1, y)
 		case tl.KeyArrowRight:
-			player.SetPosition(x+1, y)
-
+			destination.SetPosition(x+1, y)
 		}
 	}
 }
@@ -30,8 +33,11 @@ func main() {
 	level := tl.NewBaseLevel(tl.Cell{
 		Bg: tl.ColorBlack,
 	})
-	player := Player{tl.NewEntity(1, 1, 1, 1)}
-	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorRed, Ch: '옷'})
+	destination := Destination{tl.NewEntity(2, 2, 1, 1)}
+	destination.SetCell(0, 0, &tl.Cell{Fg: tl.ColorBlue, Ch: '옷'})
+	player := Player{tl.NewEntity(10, 10, 1, 1)}
+	player.SetCell(0, 0, &tl.Cell{Fg: tl.ColorYellow, Ch: '옷'})
+	level.AddEntity(&destination)
 	level.AddEntity(&player)
 	game.Screen().SetLevel(level)
 	game.Start()
